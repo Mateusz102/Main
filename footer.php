45b4f5e2b40d7197bd94c82122729ebc11b39279
Mateusz102 <mateusz.idczak.1@gmail.com>
2019-03-03 21:00:20 +0100

    hight edited

--- footer.php
+++ footer.php
@@ -4,21 +4,23 @@
         min-height:100%;
         }
         body{
-            margin-bottom: 30px;
+            margin-bottom: 35px;
         }
     .footer{
         position: absolute;
-        bottom:0;
+        bottom:0; /* dystans pomiędzy fotterem oraz doałem strony jest 0  jesli nie to albo margin <> heigh albo czcionkaza duza*/
         width:100%;
         background-color: #eaeae1;
-        height:30px;
-        /* margin-bottom: 0; */
+        height:35px;
+        font-size: 1 px;
+        text-align: center;
+        padding-top: 2px;*/
         }
 </style>
 <footer class="footer">
     <div class="container">
-        <p class="text-muted">
-            <?php
+         <p class="text-muted">
+            <?php 
             $today=date("Y");
             echo "Copyright &copy; 2018-$today Mat"
             ?>
